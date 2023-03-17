37a161a7065d0b21a52eef4fc1f1f707ab20cb79
jjolliffe28 <112976073+jjolliffe28@users.noreply.github.com>
2023-03-17 00:58:16 -0400

    Update README.py

--- README.py
+++ README.py
@@ -1,28 +1,19 @@
-import getpass
-import tableauserverclient as TSC
+import certifi
+import ssl
+from tableauserverclient import Server
 
-# Set up the authentication
-username = '<username>'
-password = getpass.getpass()
-site_id = '<site_id>'
+# specify the path to the trusted SSL certificates
+ssl_context = ssl.create_default_context(cafile=certifi.where())
 
-auth = TSC.TableauAuth(username, password, site_id=site_id)
+# create a server object and connect to the Tableau Server using username and password
+server = Server('https://yourtableauserver.com', use_server_version=True, ssl_context=ssl_context)
+server.auth.sign_in('yourusername', 'yourpassword')
 
-# Set up the server object
-server = TSC.Server('<server_url>')
+# get the workbook and download data sources as csv
+workbook = server.workbooks.get_by_name('yourworkbookname')
+for datasource in workbook.datasources:
+    csv_path = datasource.download_as_file('csv')
+    print(f"Data source '{datasource.name}' downloaded to '{csv_path}'")
 
-# Connect to the server
-with server.auth.sign_in(auth):
-    # Find the workbook by name
-    workbook_name = '<workbook_name>'
-    workbooks, _ = server.workbooks.get()
-
-    for workbook in workbooks:
-        if workbook.name == workbook_name:
-            # Download each data source in the workbook
-            for ds in workbook.published_ds:
-                ds_data = server.datasources.download(ds.id)
-                ds_file_name = f'{ds.name}.csv'
-                with open(ds_file_name, 'wb') as f:
-                    f.write(ds_data)
-                print(f'Successfully downloaded data source {ds.name} to {ds_file_name}')
+# sign out of the Tableau Server
+server.auth.sign_out()
