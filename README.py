65174e429b01ed45fba1708e04522f79021a8c4d
jjolliffe28 <112976073+jjolliffe28@users.noreply.github.com>
2023-03-24 10:55:14 -0400

    Update README.py

--- README.py
+++ README.py
@@ -1,24 +1,18 @@
-import certifi
-import ssl
-from tableauserverclient import Server
-
-# specify the path to the trusted SSL certificates
-ssl_context = ssl.create_default_context(cafile=certifi.where())
-
-# create a server object
-server = Server('https://yourtableauserver.com', use_server_version=True)
-
-# set the SSL context for the server
-server.add_http_options({'verify': ssl_context})
-
-# connect to the Tableau Server using username and password
-server.auth.sign_in('yourusername', 'yourpassword')
-
-# get the workbook and download data sources as csv
-workbook = server.workbooks.get_by_name('yourworkbookname')
-for datasource in workbook.datasources:
-    csv_path = datasource.download_as_file('csv')
-    print(f"Data source '{datasource.name}' downloaded to '{csv_path}'")
-
-# sign out of the Tableau Server
-server.auth.sign_out()
+RPI_CASTA
+  AS (
+  SELECT
+    CASE
+      WHEN RPI.ROBC_T12 < '2021-03-21' THEN NULL
+      ELSE RPI.ROBC_T12
+    END AS ROBC_T12,
+    RPI.DT,
+    RPI.EFF_DT,
+    RPI.END_DT,
+    RPI.SEGMENT,
+    RPI.OBLIGOR_LEVEL_2_NAME,
+    RPI.OBLIGOR_LEVEL_4_NAME,
+    RPI.COMMITMENTS,
+    RPI.BLENDED_CAPITAL,
+    RPI.ECONOMIC_CAPITAL,
+    RPI.GROSS_REVENUE_T12,
+    RPI.LOAN_LOSS_PROVISION_EXPENSE_T12,
