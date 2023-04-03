5c1d704d297fdb4ee5914dc0a874ec2d5bcb3c40
jjolliffe28 <112976073+jjolliffe28@users.noreply.github.com>
2023-04-03 14:03:40 -0400

    Update README.py

--- README.py
+++ README.py
@@ -7,18 +7,23 @@ client = bigquery.Client()
 # read the CSV file into a pandas data frame
 df = pd.read_csv('path/to/your/csv/file.csv')
 
-# specify the schema of the table
+# specify the schema for the "SICCode" column
 schema = [
     bigquery.SchemaField('SICCode', 'STRING'),
-    # add the schema for the other columns in the table here
 ]
+
+# get the default schema for the other columns
+default_schema = bigquery.Schema.from_dataframe(df.drop(columns=['SICCode']))
+
+# merge the schemas
+merged_schema = schema + default_schema
 
 # create a bigquery table
 table_ref = client.dataset('your_dataset').table('your_table')
-table = bigquery.Table(table_ref, schema=schema)
+table = bigquery.Table(table_ref, schema=merged_schema)
 table = client.create_table(table)
 
 # upload the data to the table
-job_config = bigquery.LoadJobConfig(schema=schema)
+job_config = bigquery.LoadJobConfig(schema=merged_schema)
 job = client.load_table_from_dataframe(df, table_ref, job_config=job_config)
 job.result()
