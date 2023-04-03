6a6a07549b54f6ce0d21ed0f5f6a512512fc3768
jjolliffe28 <112976073+jjolliffe28@users.noreply.github.com>
2023-04-03 14:00:33 -0400

    Update README.py

--- README.py
+++ README.py
@@ -1,1 +1,24 @@
-IF [Level 2] = "Real Estate" THEN [Cmt] ELSE WINDOW_SUM(SUM([Cmt])) - SUM(IF [Level 2] = "Real Estate" THEN [Cmt] END) END
+from google.cloud import bigquery
+import pandas as pd
+
+# create a bigquery client
+client = bigquery.Client()
+
+# read the CSV file into a pandas data frame
+df = pd.read_csv('path/to/your/csv/file.csv')
+
+# specify the schema of the table
+schema = [
+    bigquery.SchemaField('SICCode', 'STRING'),
+    # add the schema for the other columns in the table here
+]
+
+# create a bigquery table
+table_ref = client.dataset('your_dataset').table('your_table')
+table = bigquery.Table(table_ref, schema=schema)
+table = client.create_table(table)
+
+# upload the data to the table
+job_config = bigquery.LoadJobConfig(schema=schema)
+job = client.load_table_from_dataframe(df, table_ref, job_config=job_config)
+job.result()
