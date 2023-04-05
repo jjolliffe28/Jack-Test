10257e6b1d75ae9312abfc4553ccac6932142fef
jjolliffe28 <112976073+jjolliffe28@users.noreply.github.com>
2023-04-05 11:45:18 -0400

    Update README.py

--- README.py
+++ README.py
@@ -1,29 +1,53 @@
-from google.cloud import bigquery
-import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-# create a bigquery client
-client = bigquery.Client()
+# Define initial conditions for ball flight
+ball_speed = 150    # mph
+spin_rate = 3000    # rpm
+launch_angle = 12   # degrees
+side_spin = 0       # rpm
+back_spin = 0       # rpm
+ball_position = np.array([0, 0, 0])  # x, y, z coordinates in feet
+ball_velocity = np.array([
+    ball_speed * np.cos(np.radians(launch_angle)),  # x velocity in ft/s
+    0,  # y velocity in ft/s (assume level ground)
+    ball_speed * np.sin(np.radians(launch_angle))  # z velocity in ft/s
+])
+gravity = np.array([0, -32.2, 0])  # acceleration due to gravity in ft/s^2
+time_step = 0.01  # time step for simulation in seconds
+flight_time = 10  # total flight time in seconds
 
-# read the CSV file into a pandas data frame
-df = pd.read_csv('path/to/your/csv/file.csv')
+# Create an array of time values
+time = np.arange(0, flight_time, time_step)
 
-# specify the schema for the "SICCode" column
-schema = [
-    bigquery.SchemaField('SICCode', 'STRING'),
-]
+# Create arrays to store ball position and velocity at each time step
+position = np.zeros((len(time), 3))
+velocity = np.zeros((len(time), 3))
+position[0] = ball_position
+velocity[0] = ball_velocity
 
-# get the default schema for the other columns
-default_schema = bigquery.Schema.from_dataframe(df.drop(columns=['SICCode']))
+# Simulate the ball flight
+for i in range(1, len(time)):
+    # Calculate the total spin of the ball
+    spin = np.sqrt(spin_rate**2 + side_spin**2 + back_spin**2)
+    
+    # Calculate the Magnus force on the ball due to spin
+    magnus_force = 0.5 * np.pi * 0.225 * spin / 1000 * np.cross(velocity[i-1], [spin_rate, side_spin, back_spin])
+    
+    # Calculate the acceleration on the ball due to gravity and Magnus force
+    acceleration = gravity + magnus_force / 0.04593
+    
+    # Update the ball velocity and position
+    velocity[i] = velocity[i-1] + acceleration * time_step
+    position[i] = position[i-1] + velocity[i] * time_step
 
-# merge the schemas
-merged_schema = schema + default_schema
-
-# create a bigquery table
-table_ref = client.dataset('your_dataset').table('your_table')
-table = bigquery.Table(table_ref, schema=merged_schema)
-table = client.create_table(table)
-
-# upload the data to the table
-job_config = bigquery.LoadJobConfig(schema=merged_schema)
-job = client.load_table_from_dataframe(df, table_ref, job_config=job_config)
-job.result()
+# Plot the ball trajectory
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(position[:, 0], position[:, 1], position[:, 2], label='Ball Trajectory')
+ax.set_xlabel('X Position (ft)')
+ax.set_ylabel('Y Position (ft)')
+ax.set_zlabel('Z Position (ft)')
+plt.legend()
+plt.show()
