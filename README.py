bb41429718a8e03d303d9af456348eff7d91742d
jjolliffe28 <112976073+jjolliffe28@users.noreply.github.com>
2023-04-17 12:16:10 -0400

    Update README.py

--- README.py
+++ README.py
@@ -4,3 +4,9 @@ SUM(IF DATEPART('quarter', [Order Date]) = DATEPART('quarter', DATEADD('quarter'
 
 SUM(IF DATEPART('year', [Order Date]) = DATEPART('year', TODAY()) THEN [Sales] END) -
 SUM(IF DATEPART('year', [Order Date]) = DATEPART('year', DATEADD('year', -1, TODAY())) THEN [Sales] END)
+
+
+TOPN(3, 
+    {FIXED [Quarter]: SUM(IF DATEDIFF('quarter', [Order Date], TODAY()) = 0, [Commitments], 0)} 
+    - {FIXED [Quarter]: SUM(IF DATEDIFF('quarter', [Order Date], TODAY()) = 1, [Commitments], 0)}
+    , 1)
