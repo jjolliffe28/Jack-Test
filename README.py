0117875eaea1043f959ab428ad990d0ba0e6afa9
jjolliffe28 <112976073+jjolliffe28@users.noreply.github.com>
2023-04-28 15:02:49 -0400

    Update README.py

--- README.py
+++ README.py
@@ -1,21 +1,32 @@
+import pandas as pd
+
+# create the sample dataframe
+data = {'name': ['John', 'John', 'John', 'Alex', 'Alex', 'Alex'],
+        'flag': ['Y', 'N', 'N', 'Y', 'N', 'Y'],
+        'date': ['2022-01-01', '2022-02-01', '2022-03-01', '2022-01-01', '2022-02-01', '2022-03-01']}
+df = pd.DataFrame(data)
+
+# convert the date column to datetime format
+df['date'] = pd.to_datetime(df['date'])
+
 # sort the dataframe by name and date
-df = df.sort_values(['name', 'date'], ascending=[True, False])
+df = df.sort_values(['name', 'date'])
 
 # create an empty dictionary to store the last change dates
 last_change_dates = {}
 
-# iterate over each group of the dataframe in reverse order
-for name, group in df.groupby('name', sort=False).apply(lambda x: x.iloc[::-1]):
-    # get the flag values and dates of the group
-    flags = group['flag'].values
-    dates = group['date'].values
+# iterate over each unique name in the dataframe
+for name in df['name'].unique():
+    # get the flag values and dates for the name
+    flags = df[df['name'] == name]['flag'].values
+    dates = df[df['name'] == name]['date'].values
     
-    # iterate over each pair of adjacent rows
-    for i in range(len(flags)-1):
+    # iterate over each pair of adjacent rows in reverse order
+    for i in range(len(flags)-1, 0, -1):
         # check if the flag values are different
-        if flags[i] != flags[i+1]:
+        if flags[i] != flags[i-1]:
             # if they are different, add the last change date to the dictionary
-            last_change_dates[name] = dates[i+1]
+            last_change_dates[name] = dates[i]
             break
     
     # if the last change date is not found, add None to the dictionary
@@ -23,9 +34,6 @@ for name, group in df.groupby('name', sort=False).apply(lambda x: x.iloc[::-1]):
         last_change_dates[name] = None
 
 # create a new column in the dataframe to store the last change dates
-df['last_change_date'] = df['name'].apply(lambda x: last_change_dates.get(x, None))
-
-# sort the dataframe back to its original order
-df = df.sort_index()
+df['last_change_date'] = df['name'].apply(lambda x: last_change_dates[x])
 
 print(df)
