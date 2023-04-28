a36b868056f456a348b78a96ffff959121cedb67
jjolliffe28 <112976073+jjolliffe28@users.noreply.github.com>
2023-04-28 14:57:15 -0400

    Update README.py

--- README.py
+++ README.py
@@ -1,19 +1,29 @@
+# sort the dataframe by name and date
+df = df.sort_values(['name', 'date'], ascending=[True, False])
+
+# create an empty dictionary to store the last change dates
 last_change_dates = {}
 
-# loop through each row of the dataframe
-for index, row in df.iterrows():
-    # check if the name is already in the dictionary
-    if row['name'] in last_change_dates:
-        # check if the flag has changed
-        if row['flag'] != last_change_dates[row['name']][0]:
-            # if flag has changed, update the dictionary with the new date
-            last_change_dates[row['name']] = (row['flag'], row['date'])
-    else:
-        # if name is not in the dictionary, add it with current flag and date
-        last_change_dates[row['name']] = (row['flag'], row['date'])
+# iterate over each group of the dataframe in reverse order
+for name, group in df.groupby('name', sort=False).apply(lambda x: x.iloc[::-1]):
+    # get the flag values and dates of the group
+    flags = group['flag'].values
+    dates = group['date'].values
+    
+    # iterate over each pair of adjacent rows
+    for i in range(len(flags)-1):
+        # check if the flag values are different
+        if flags[i] != flags[i+1]:
+            # if they are different, add the last change date to the dictionary
+            last_change_dates[name] = dates[i+1]
+            break
+    
+    # if the last change date is not found, add None to the dictionary
+    if name not in last_change_dates:
+        last_change_dates[name] = None
 
 # create a new column in the dataframe to store the last change dates
-df['last_change_date'] = df['name'].apply(lambda x: last_change_dates[x][1])
+df['last_change_date'] = df['name'].apply(lambda x: last_change_dates.get(x, None))
 
 # sort the dataframe back to its original order
 df = df.sort_index()
